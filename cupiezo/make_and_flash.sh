096ce9cf6a03c33ebf9e0610cd034c72291a6c37
sanjeevprasad <this.sanjeevprasad@gmail.com>
2023-10-18 13:43:19 +0530

    updated_readme

--- cupiezo/make_and_flash.sh
+++ cupiezo/make_and_flash.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+# this file waits endlessly for the rp2040 to be plugged in and
+# will try to mount it automatically when it is boot mode.
+# and will flash the firmware, when done, it will unmount the device.
+
+# you may want to change this DISK env var according to you device.
+# DON'T use /dev/sdX syntax because that is not consistent. and you MAY accidentally
+# format your Data and flash it.
+
 BIN=$1
 DISK=/dev/disk/by-id/usb-RPI_RP2_E0C912952D54-0:0-part1
-cargo build --bin $BIN --release || exit 1
 while true; do
   echo Waiting for rp2040
   while [ ! -e $DISK ]; do
@@ -9,6 +16,7 @@ while true; do
   done
   sudo umount /mnt
   sudo mount $DISK /mnt/ -o uid=1000,gid=1000 || exit 1
+  cargo build --release --bin $BIN
   elf2uf2-rs -d target/thumbv6m-none-eabi/release/$BIN && sudo umount /mnt
   sudo umount /mnt
   sleep 1;
--- cupiezo/readme.md
+++ cupiezo/readme.md
@@ -3,3 +3,15 @@
 ### Rust driver for bos1901 (for now). to drive and read piezo.
 
 This is an early phase. so things may or may not work.
+
+build steps
+```sh
+cargo build --release
+# modify the file to use your chipid before flashing
+# ./make_and_flash.sh name_of_the_bin
+# example
+# ./make_and_flash.sh main
+```
+
+`rnd/` directory only contains experiment code. we don't need that.
+
